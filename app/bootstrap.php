fe118b5d0632064095ad1d1e7a0fbb9b2c13ddbe
Tomáš Vítek <me@tomasvitek.com>
2016-12-09 12:54:21 +0000

    Disable debugger

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,8 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode([127.0.0.1]); // enables debugger for given ips
-$configurator->setDebugMode(TRUE); // enables debugger for all
+// $configurator->setDebugMode([127.0.0.1]); // enables debugger for given ips
+// $configurator->setDebugMode(TRUE); // enables debugger for all
 
 $configurator->enableDebugger(__DIR__ . '/../tmp/log');
 $configurator->setTempDirectory(__DIR__ . '/../tmp');
